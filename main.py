6df04c8ae48fba0409e2f21ace9f557d11366a29
user <kudakovazam@yandex.ru>
2023-04-24 13:15:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,99 @@
+import sys
+import pygame
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+pygame.init()
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 480
+MAP_SIZE = 20
+TILE_SIZE = SCREEN_WIDTH / MAP_SIZE
+MOUSE_DOWN = False
+
+START = (0,0)
+FINISH = (17, 17)
+G = nx.Graph()
+adj_matrix = np.zeros((MAP_SIZE ** 2, MAP_SIZE  ** 2))
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+WINDOW = pygame.surface.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+MAP = []
+
+for i in range(MAP_SIZE):
+    row = []
+    for j in range(MAP_SIZE):
+        if (i, j) == START:
+            cell = '#'
+        elif (i, j) == FINISH:
+            cell = '@'
+        else:
+            cell = ''
+        row.append(cell)
+    MAP.append(row)
+
+def get_graph(MAP):
+    x = MAP_SIZE
+    y = MAP_SIZE
+    for i in range(x):
+        for j in range(y):
+            vertex = i * y + j  # Номер вершины в матрице смежности
+            for r in range(max(0, i - 1), min(x, i + 2)):
+                for c in range(max(0, j - 1), min(y, j + 2)):
+                    if r == i and c == j:
+                        continue
+                    neighbor = r * y + c  # Номер соседней вершины в матрице смежности
+                    if MAP[r][c] == '.':
+                        adj_matrix[vertex][neighbor] = 0
+                    else:
+                        adj_matrix[vertex][neighbor] = 1
+        print(adj_matrix)
+
+def draw_map(WINDOW, TILE_SIZE, MAP):
+    wall_color = (190, 190, 190)
+    space_color = (65, 65, 65)
+
+    for i in range(MAP_SIZE):
+        for j in range(MAP_SIZE):
+            cell = MAP[i][j]
+            if cell == '#':
+                color = (0, 0, 190)
+            elif cell == '@':
+                color = (0, 190, 0)
+            else:
+                color = space_color if MAP[i][j] == '' else wall_color
+            pygame.draw.rect(WINDOW, color, (TILE_SIZE * i, TILE_SIZE * j, TILE_SIZE - 1, TILE_SIZE - 1))
+
+get_graph(MAP)
+
+while True:
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            MOUSE_DOWN = True
+            x, y = pygame.mouse.get_pos()
+            cell_x = int(x // TILE_SIZE)
+            cell_y = int(y // TILE_SIZE)
+            MAP[cell_x][cell_y] = '.'
+            get_graph(MAP)
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            MOUSE_DOWN = False
+
+        if event.type == pygame.MOUSEMOTION and MOUSE_DOWN:
+            x, y = pygame.mouse.get_pos()
+            cell_x = int(x // TILE_SIZE)
+            cell_y = int(y // TILE_SIZE)
+            MAP[cell_x][cell_y] = '.'
+            get_graph(MAP)
+
+    draw_map(WINDOW, TILE_SIZE, MAP)
+
+    screen.blit(WINDOW, (0,0))
+    pygame.display.flip()
+    clock.tick(60)
---
+++ test.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+x = 3
+y = 3
+# Создаем матрицу [x][y]
+matrix = np.random.randint(0, 2, size=(x, y))
+
+# Создаем матрицу смежности размером [x][y]
+adj_matrix = np.zeros((x*y, x*y))
+
+# Заполняем матрицу смежности
+for i in range(x):
+    for j in range(y):
+        vertex = i * y + j  # Номер вершины в матрице смежности
+        for r in range(max(0, i-1), min(x, i+2)):
+            for c in range(max(0, j-1), min(y, j+2)):
+                if r == i and c == j:
+                    continue
+                neighbor = r * y + c  # Номер соседней вершины в матрице смежности
+                if matrix[r][c] == '.':
+                    adj_matrix[vertex][neighbor] = 20000000
+                else:
+                    adj_matrix[vertex][neighbor] = 1
+print(adj_matrix)
