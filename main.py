972c8a2e32b1dd9030bcc9d12b21f74d2d35fc99
user <kudakovazam@yandex.ru>
2023-04-24 14:13:00 +0300

    asd

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ import matplotlib.pyplot as plt
 pygame.init()
 SCREEN_WIDTH = 480
 SCREEN_HEIGHT = 480
-MAP_SIZE = 20
+MAP_SIZE = 3
 TILE_SIZE = SCREEN_WIDTH / MAP_SIZE
 MOUSE_DOWN = False
-
 START = (0,0)
 FINISH = (17, 17)
 G = nx.Graph()
@@ -20,6 +19,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 WINDOW = pygame.surface.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 MAP = []
+
 
 for i in range(MAP_SIZE):
     row = []
@@ -34,21 +34,30 @@ for i in range(MAP_SIZE):
     MAP.append(row)
 
 def get_graph(MAP):
+    for i in range(len(adj_matrix)):
+        for j in range(i + 1, len(adj_matrix)):
+            if adj_matrix[i][j] == 1:
+                G.add_edge(i, j)
+    nx.draw(G, with_labels=True)
+    plt.show()
+
+def get_adj(MAP):
     x = MAP_SIZE
     y = MAP_SIZE
     for i in range(x):
         for j in range(y):
-            vertex = i * y + j  # Номер вершины в матрице смежности
+            vertex = i * y + j# Номер вершины в матрице смежности
             for r in range(max(0, i - 1), min(x, i + 2)):
                 for c in range(max(0, j - 1), min(y, j + 2)):
                     if r == i and c == j:
                         continue
-                    neighbor = r * y + c  # Номер соседней вершины в матрице смежности
-                    if MAP[r][c] == '.':
-                        adj_matrix[vertex][neighbor] = 0
-                    else:
-                        adj_matrix[vertex][neighbor] = 1
-        print(adj_matrix)
+                    neighbor = r * y + c # Номер соседней вершины в матрице смежности
+                    if abs((vertex - r) + (neighbor - c)) != 3 :
+                        if MAP[r][c] == '.':
+                            adj_matrix[vertex][neighbor] = 0
+                        else:
+                            adj_matrix[vertex][neighbor] = 1
+    print(adj_matrix)
 
 def draw_map(WINDOW, TILE_SIZE, MAP):
     wall_color = (190, 190, 190)
@@ -65,7 +74,11 @@ def draw_map(WINDOW, TILE_SIZE, MAP):
                 color = space_color if MAP[i][j] == '' else wall_color
             pygame.draw.rect(WINDOW, color, (TILE_SIZE * i, TILE_SIZE * j, TILE_SIZE - 1, TILE_SIZE - 1))
 
+get_adj(MAP)
 get_graph(MAP)
+
+for i in range(len(adj_matrix)):
+    G.add_node(i)
 
 while True:
     events = pygame.event.get()
@@ -80,7 +93,7 @@ while True:
             cell_x = int(x // TILE_SIZE)
             cell_y = int(y // TILE_SIZE)
             MAP[cell_x][cell_y] = '.'
-            get_graph(MAP)
+            get_adj(MAP)
 
         if event.type == pygame.MOUSEBUTTONUP:
             MOUSE_DOWN = False
@@ -90,6 +103,8 @@ while True:
             cell_x = int(x // TILE_SIZE)
             cell_y = int(y // TILE_SIZE)
             MAP[cell_x][cell_y] = '.'
+            get_adj(MAP)
+        if event.type == pygame.K_SPACE:
             get_graph(MAP)
 
     draw_map(WINDOW, TILE_SIZE, MAP)
